89526719420a4b21ea1e9b159ffd15026fb6c684
IndieWise <130398443+IndieWise@users.noreply.github.com>
2023-06-17 20:21:39 -0300

    Começando a fazer sistema de grupos

---
+++ src/main/java/com/api/indiewise/models/CommunityModel.java
@@ -0,0 +1,35 @@
+package com.api.indiewise.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "community")
+public class CommunityModel {
+    @Id
+    private String id;
+    private String nome;
+    private String descricao;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
