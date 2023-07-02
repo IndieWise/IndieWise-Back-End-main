3526f803bb1a2585eeb7ffffe3306f1cdd40bd7e
IndieWise <130398443+IndieWise@users.noreply.github.com>
2023-07-01 21:14:13 -0300

    atualizaçõa

--- src/main/java/com/api/indiewise/controllers/ViewController.java
+++ src/main/java/com/api/indiewise/controllers/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/teste")
+    @GetMapping("/indiewise.com.br")
     public String home() {
         return "login.html";
     }
