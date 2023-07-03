c2ecc3c8dde127a99026710252b84c1690afbfc7
Mailson Costa <mailsoncosta2ba2@gmail.com>
2023-07-03 13:00:57 -0300

    Atualização da pasta static

--- src/main/java/com/api/indiewise/controllers/ViewController.java
+++ src/main/java/com/api/indiewise/controllers/ViewController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ViewController {
     @GetMapping("/indiewise.com.br")
     public String home() {
-        return "login.html";
+        return "index.html";
     }
 }
